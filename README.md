2cfc99b30f38e266155ec105865fad1f0a37d78a
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-12-12 18:48:13 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hdl
-Handball Data
+- 👋 Hi, I’m @uri172192
+- 👀 I’m interested in Handball Data
+- 🌱 I’m currently learning Data Analytics
+
+
+<!---
+uri172192/uri172192 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
---
+++ 🤾Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+#-----------------------------------------------
+st.set_page_config(page_title="HDL", page_icon="favicon-32x32.png", layout="wide")
+
+image = Image.open('HDL-blanc.png')
+st.image(image) 
+
+
+st.subheader('📌Descripción HDL')
+st.write('📢**Handball Data Lab** se presenta como una aplicación destinada al desarrollo y democratización del análisis de datos en balonmano. La finalidad es ayudar a los usarios a **disfrutar, comprender y compartir los datos sobre el balonmano**.')
+    
+st.divider()
+st.subheader("📌Contenidos HDL")
+st.write("🏐**Scorers**: visualiza los goleadores según equipo y posición")
+st.write("🏹**Shooting Distances**: explora los máximos anotadores según la distancia del lanzamiento")
+st.write("🎯**Players Shooting Performance**: escoge 2 jugadores y compara su rendimiento en el lanzamiento")
+st.write("📋**Efficiency Snapshot Asobal**: conoce como han rendido los equipos durante la temporada")
+st.write("🕵️**Shooting Similiraty**: descubre los jugadores similares entre si según su eficacia en el lanzamiento")
+st.write("🗂️**Data Consulting**: consulta los datos de los que disponemos sobre cada equipo en materia de lanzamientos")
+st.divider()
+
+
