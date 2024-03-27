c2b420602710c7917c8af0b722884c3bb588efcc
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-03-27 14:19:26 +0100

    gkgpehf24.py

--- gkgpehf24.py
+++ gkgpehf24.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="TEST GK", layout="wide")
 st.markdown("<h1 style='text-align: center;'>Goalkeeper EHF Group Phase</h1>", unsafe_allow_html=True)
 
 left_co, center_co, last_co = st.columns(3)
-with last_co:
+with center_co:
    image_ehf_logo = Image.open('ehflogo.png')
    st.image(image_ehf_logo)
 
