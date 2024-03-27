e14ff086c01542a887cc6212c108d0dec8db31dc
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-03-27 17:41:34 +0100

    gkgpehf24.py

--- gkgpehf24.py
+++ gkgpehf24.py
@@ -26,11 +26,11 @@ with expander:
     # Mostrar el DataFrame filtrado si se seleccionó un equipo
     if not equipo_df.empty:
         #st.write(f'GK Data EHF Team Selected: {gkteam}:')
-        rounded_df1 = st.write(equipo_df)
-        # Redondear los valores del DataFrame seleccionado y mostrarlo en Streamlit
-        st.write("", rounded_df1)
+        st.write(equipo_df)
     else:
         st.warning('Please, select an EHF team.')
+
+
 
 # Crear el expander
 expander = st.expander("➕ **EHF GK INDIVIDUAL DATA CONSULTORY** 🥅🤾🏻‍♂️")
@@ -45,9 +45,12 @@ with expander:
 
     if not gk_selected.empty:
         #st.write(f'GK Data EHF Team {gk}:')
-        roundad_df2 = st.write(gk_selected)
-        # Redondear los valores del DataFrame seleccionado y mostrarlo en Streamlit
-        st.write("", rounded_df2)
+        #st.write(gk_selected)
+        # Redondear los valores a 2 decimales
+        gk_selected_rounded = gk_selected.round(2)
+        
+        #st.write(f'GK Data EHF Team {gk}:')
+        st.write(gk_selected_rounded)
     else:
         st.warning('Please, write a GK Name.')
 
