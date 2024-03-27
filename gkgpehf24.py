b0eac9c8f4413539b68d637d9f5e78630590b4c5
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-03-27 13:39:37 +0100

    Add files via upload

---
+++ gkgpehf24.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(page_title="TEST GK", layout="wide")
+st.markdown("<h1 style='text-align: center;'>GK EHF Group Phase</h1>", unsafe_allow_html=True)
+
+df = pd.read_excel("C:/Users/Lenovo/Downloads/Sports Data Analyst/Handball Analytics/EHF Champions League/dfgkgp.xlsx")
+
+# Crear el expander
+expander = st.expander("➕ **EHF GK TEAMS CONSULTORY**")
+
+# Dentro del expander, agregar el código para seleccionar un jugador
+with expander:
+    gkteam = st.selectbox('Select your team:',
+                        (df['Team']),
+                        index=None,
+                        placeholder="Select EHF team...")
+    st.write('EHF Team selected:', gkteam)
+
+    # Filtrar el DataFrame según el equipo seleccionado
+    equipo_df = df[df['Team'] == gkteam]
+
+    # Mostrar el DataFrame filtrado si se seleccionó un equipo
+    if not equipo_df.empty:
+        st.write(f'GK Data EHF Team {gkteam}:')
+        st.write(equipo_df)
+    else:
+        st.warning('Please, select an EHF team.')
+
+# Crear el expander
+expander = st.expander("➕ **EHF GK INDIVIDUAL CONSULTORY**")
+
+# Dentro del expander, agregar el código para seleccionar un jugador
+with expander:
+    gk = st.selectbox('Select EHF GK:', (df['Name']), index=None, placeholder='Type GK Name...')
+    gk_selected = df[df['Name'] == gk]
+
+    if not gk_selected.empty:
+        st.write(f'GK Data EHF Team {gk}:')
+        st.write(gk_selected)
+    else:
+        st.warning('Please, write a GK Name.')
+
+
+# Mostrar el DataFrame completo + Search Bar:
+st.write('GK EHF 23/24 Group Phase:', df)
+
+
+st.caption("🔎Source: EHF")
+expander = st.expander(" ➕ **LEGEND**")
+expander.write("**GC** = Goals Conceded")
+expander.write("**NºMSA** = Number of Saves made by X distance")
+expander.write("**NºMSO** = Number of Shots received by X distance")
+expander.write("**WSA** = Number of saves made from wing shots")
+expander.write("**WSO** = Number of wing shots received")
