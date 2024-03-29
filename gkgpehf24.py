79a0e7334f4e9cd0147fef607c07090bdb4e16ff
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2024-03-29 14:22:35 +0100

    Update gkgpehf24.py

--- gkgpehf24.py
+++ gkgpehf24.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+from soccerplots.radar_chart import Radar
 
 st.set_page_config(page_title="GK EHF DATA", page_icon="favicon-32x32.png", layout="wide")
 
@@ -65,6 +66,53 @@ with expander:
         st.write(gk_selected_rounded)
     else:
         st.warning('Please, write a GK Name.')
+
+
+
+## parameter names
+params = ['xAssists', 'Key Passes', 'Crosses Into Box', 'Cross Competion', 'Deep Completions', 
+          'Progressive Passes', 'Prog. Passes Accuracy%', 'Dribbles', 'Progressive Runs', 
+          'PADJ Interceptions', 'Succ. Def Actions', 'Def Duel Win%']
+
+## range values
+ranges = [(0.0, 0.15), (0.0, 0.67), (0.06, 0.63), (19.51, 50.0), (0.35, 1.61), (6.45, 11.94), (62.94, 79.46), (0.43, 4.08), (0.6, 2.33), (5.01, 7.2), (9.02, 12.48),
+          (52.44, 66.67)]
+
+## parameter value
+values = [
+    [0.11, 0.53, 0.70, 27.66, 1.05, 6.84, 84.62, 4.56, 2.22, 5.93, 8.88, 64.29],   ## for Sergino Dest
+    [0.07, 0.36, 0.16, 32.14, 1.04, 7.37, 74.46, 3.68, 2.40, 6.87, 8.97, 61.14]    ## for Nelson Semedo
+]
+
+## title
+title = dict(
+    title_name='Sergiño Dest',
+    title_color='#B6282F',
+    subtitle_name='AFC Ajax',
+    subtitle_color='#B6282F',
+    title_name_2='Nelson Semedo',
+    title_color_2='#344D94',
+    subtitle_name_2='Barcelona',
+    subtitle_color_2='#344D94',
+    title_fontsize=18,
+    subtitle_fontsize=15,
+)
+
+## endnote 
+endnote = "Visualization made by: Anmol Durgapal(@slothfulwave612)\nAll units are in per90"
+
+## instantiate object
+radar = Radar()
+
+## plot radar -- compare
+fig, ax = radar.plot_radar(ranges=ranges, params=params, values=values, 
+                           radar_color=['#B6282F', '#344D94'], 
+                           title=title, endnote=endnote,
+                           compare=True)
+
+
+
+
 
 
 # Mostrar el DataFrame completo + Search Bar:
